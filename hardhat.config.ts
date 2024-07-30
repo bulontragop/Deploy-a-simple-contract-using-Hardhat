497beb50662de976514f0d5284f7ba348caf3b08
bulontragop <minhdungansc@gmail.com>
2024-07-30 06:31:51 +0000

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,7 +9,7 @@ const config: HardhatUserConfig = {
   networks: {
     swisstronik: {
       url: 'https://json-rpc.testnet.swisstronik.com/', //URL of the RPC node for Swisstronik.
-      accounts: [`0x${process.env.PRIVATE_KEY}`], //Your private key starting with "0x"
+      accounts: [`0xabc`], //Your private key starting with "0x"
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,3 +1,3 @@
-const deployedAddress = ''
+const deployedAddress = '0xfBE4a48aa17B343743F74D5FF9B6fE0681c2D230'
 
 export default deployedAddress
